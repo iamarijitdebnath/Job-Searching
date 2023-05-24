94478b19046ae0723cc7ad59dbf437714afc4a65
Arijit Debnath <arijitdebnath34@gmail.com>
2023-05-25 02:47:47 +0530

    nothing

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello</h1>")
+    # return HttpResponse("<h1>Hello</h1>")
     category=""
     location=""
     print("showing....")
