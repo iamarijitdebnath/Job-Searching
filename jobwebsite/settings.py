a22a3b3557d220be7f1dcb1f86416ba69de9f514
Arijit Debnath <arijitdebnath34@gmail.com>
2023-05-24 23:05:27 +0530

    done vercel setting

--- jobwebsite/settings.py
+++ jobwebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oekuba3^4oo1p-24b)h&q(=fqi!b@atlr)v@9lp^rh1pm(#es+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -133,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
