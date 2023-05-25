58247299f15a63eb2fc3d4f69d5c691bcba31030
Arijit Debnath <arijitdebnath34@gmail.com>
2023-05-26 00:59:11 +0530

    changed static root

--- jobwebsite/settings.py
+++ jobwebsite/settings.py
@@ -139,4 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
