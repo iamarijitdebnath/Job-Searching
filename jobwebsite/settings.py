1f50b3002d0a25cb2ab3c7e7a16158858c5955ac
Arijit Debnath <arijitdebnath34@gmail.com>
2023-05-26 01:02:48 +0530

    modified settings

--- jobwebsite/settings.py
+++ jobwebsite/settings.py
@@ -140,4 +140,4 @@ import os
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles_build"
